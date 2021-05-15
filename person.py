d9f07170f8305994cb59c6f04fc7b5fe92c0978a
erikaosgue <1476@holbertonschool.com>
2021-05-15 16:09:51 -0400

    Add function that prints the name of the person with a message

--- person.py
+++ person.py
@@ -8,6 +8,9 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def myfunc(self):
+        print("Hello my name is " + self.name)
 
 
 person1 = Person("Amanda", 22)
