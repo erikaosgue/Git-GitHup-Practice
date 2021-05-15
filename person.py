a2a748fc2f6600f868ee9dc27adca97ba31de5d1
erikaosgue <1476@holbertonschool.com>
2021-05-15 15:11:17 -0400

    Create class Person

---
+++ person.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Exercise to practice git with a simple class
+
+Class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+person1 = Person("Amanda", 22)
+
+#ADD THIS TO 6eabe54 commit
+# Print the name and the age of the Person
+print(person1.name)
